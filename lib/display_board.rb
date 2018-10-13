83008d63518d14709885c75379b3f74cecae8e69
Marc-Clemens Lange <mrc.clemens.lange@gmail.com>
2018-10-13 18:03:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,12 @@
 # out the current state.
 
 board = [" "," "," "," "," "," "," "," "," "]
-rows = [" #|   |   "]
+rows = [" #{board[0]} | #{board[2]} | #{board[3]} ", "-----------", " #{board[4]} | #{board[5]} | #{board[6]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "]
 
 def display_board(board)
   puts rows[0]
   puts rows[1]
   puts rows[2]
+  puts rows[3]
+  puts rows[4]
 end
